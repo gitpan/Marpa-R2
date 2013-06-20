76f963161341cee451369ea065e6212f3537f0ac
Jeffrey Kegler <jkegl@cpan.org>
2013-06-20 03:13:49 +0000

    Import of JKEGL/Marpa-R2-2.059_000 from CPAN.
    gitpan-cpan-distribution: Marpa-R2
    gitpan-cpan-version:      2.059_000
    gitpan-cpan-path:         JKEGL/Marpa-R2-2.059_000.tar.gz
    gitpan-cpan-author:       JKEGL
    gitpan-cpan-maturity:     developer

--- lib/Marpa/R2/Internal.pm
+++ lib/Marpa/R2/Internal.pm
@@ -21,7 +21,7 @@ use warnings;
 use Carp;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.058000';
+$VERSION        = '2.059_000';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
