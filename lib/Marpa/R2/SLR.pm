9a735f51f7af006040d38d9cd8e9b2faf72aed63
Jeffrey Kegler <jkegl@cpan.org>
2014-08-09 04:09:17 +0000

    Import of JKEGL/Marpa-R2-2.089_001 from CPAN.
    gitpan-cpan-distribution: Marpa-R2
    gitpan-cpan-version:      2.089_001
    gitpan-cpan-path:         JKEGL/Marpa-R2-2.089_001.tar.gz
    gitpan-cpan-author:       JKEGL
    gitpan-cpan-maturity:     developer

--- lib/Marpa/R2/SLR.pm
+++ lib/Marpa/R2/SLR.pm
@@ -20,7 +20,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '2.088000';
+$VERSION        = '2.089_001';
 $STRING_VERSION = $VERSION;
 ## no critic(BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
@@ -449,6 +449,7 @@ my $libmarpa_trace_event_handlers = {
         say {$trace_file_handle} qq{Lexer "$lexer_name" accepted lexeme },
             input_range_describe( $slr, $lexeme_start_pos,
             $lexeme_end_pos - 1 ),
+            q{ e}, $slr->current_g1_location(),
             q{: },
             $thick_g1_grammar->symbol_in_display_form($g1_lexeme),
             qq{; value="$raw_token_value"}
@@ -572,6 +573,7 @@ my $libmarpa_trace_event_handlers = {
             'Attempting to read lexeme ',
             input_range_describe( $slr, $lexeme_start_pos,
             $lexeme_end_pos - 1 ),
+            q{ e}, $slr->current_g1_location(),
             q{: },
             $thick_g1_grammar->symbol_in_display_form($g1_lexeme),
             qq{; value="$raw_token_value"}
